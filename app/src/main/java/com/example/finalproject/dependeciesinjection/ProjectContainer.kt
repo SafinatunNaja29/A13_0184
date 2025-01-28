d308f61dae405ba23dc76931b46a9bc36bc6f459
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 10:34:57 +0700

    membuat class ProjectContainer

--- app/src/main/java/com/example/finalproject/dependeciesinjection/ProjectContainer.kt
+++ app/src/main/java/com/example/finalproject/dependeciesinjection/ProjectContainer.kt
@@ -1,10 +1,57 @@
 package com.example.finalproject.dependeciesinjection
 
+import com.example.finalproject.repository.NetworkPesertaRepository
 import com.example.finalproject.repository.PesertaRepository
+import com.example.finalproject.service.PesertaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val eventRepository: EventRepository
     val pesertaRepository : PesertaRepository
     val tiketRepository : TiketRepository
     val transaksiRepository : TransaksiRepository
+}
+
+class ProjectContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2:80/umyTI/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+
+    private val eventService : EventService by lazy {
+        retrofit.create(EventService::class.java)
+    }
+
+    override val eventRepository: EventRepository by lazy {
+        NetworkEventRepository(eventService)
+    }
+
+    private val pesertaService : PesertaService by lazy {
+        retrofit.create(PesertaService::class.java)
+    }
+
+    override val pesertaRepository: PesertaRepository by lazy {
+        NetworkPesertaRepository(pesertaService)
+    }
+
+    private val tiketService : TiketService by lazy {
+        retrofit.create(TiketService::class.java)
+    }
+
+    override val tiketRepository: TiketRepository by lazy {
+        NetworkTiketRepository(tiketService)
+    }
+
+    private val transaksiService : TransaksiService by lazy {
+        retrofit.create(TransaksiService::class.java)
+    }
+
+    override val transaksiRepository: TransaksiRepository by lazy {
+        NetworkTransaksiRepository(transaksiService)
+    }
 }
