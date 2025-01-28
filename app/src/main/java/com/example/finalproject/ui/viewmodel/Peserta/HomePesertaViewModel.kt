cabff4236330f7eaea0564788ee11f1863f7c5ff
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 09:41:11 +0700

    menambahkan class HomePesertaViewModel dengan funget dan fundelete

--- app/src/main/java/com/example/finalproject/ui/viewmodel/Peserta/HomePesertaViewModel.kt
+++ app/src/main/java/com/example/finalproject/ui/viewmodel/Peserta/HomePesertaViewModel.kt
@@ -1,9 +1,52 @@
 package com.example.finalproject.ui.viewmodel.Peserta
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.finalproject.model.Peserta
+import com.example.finalproject.repository.PesertaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomePesertaUiState{
     data class Success(val peserta: List<Peserta>): HomePesertaUiState()
     object Loading: HomePesertaUiState()
     object Error: HomePesertaUiState()
+}
+
+class HomePesertaViewModel(private val peserta: PesertaRepository): ViewModel() {
+    var pesertaUiState: HomePesertaUiState by mutableStateOf(HomePesertaUiState.Loading)
+        private set
+
+    init {
+        getPeserta()
+    }
+
+    fun getPeserta(){
+        viewModelScope.launch {
+            pesertaUiState = HomePesertaUiState.Loading
+            pesertaUiState = try{
+                HomePesertaUiState.Success(peserta.getPeserta())
+            }catch (e: IOException){
+                HomePesertaUiState.Error
+            }catch (e: HttpException){
+                HomePesertaUiState.Error
+            }
+        }
+    }
+
+    fun deletePeserta(id_peserta: String){
+        viewModelScope.launch {
+            try{
+                peserta.deletePeserta(id_peserta)
+            }catch (e: IOException){
+                HomePesertaUiState.Error
+            }catch (e: HttpException){
+                HomePesertaUiState.Error
+            }
+        }
+    }
 }
