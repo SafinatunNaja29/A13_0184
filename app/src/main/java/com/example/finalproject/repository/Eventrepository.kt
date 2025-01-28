fbc107d9f28886f4e1dbfac1562d15f06edd9cca
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 12:19:58 +0700

    membuat interface EventRepository

--- app/src/main/java/com/example/finalproject/repository/Eventrepository.kt
+++ app/src/main/java/com/example/finalproject/repository/Eventrepository.kt
@@ -1,6 +1,8 @@
 package com.example.finalproject.repository
 
 import com.example.finalproject.model.Event
+import com.example.finalproject.service.EventService
+import okio.IOException
 
 interface EventRepository{
     suspend fun getEvent(): List<Event>
@@ -8,4 +10,39 @@ interface EventRepository{
     suspend fun updateEvent(id_event: String, event: Event)
     suspend fun deleteEvent(id_event: String)
     suspend fun getEventById(id_event: String): Event
+}
+
+class NetworkEventRepository(
+    private val eventApiService: EventService
+): EventRepository {
+    override suspend fun getEvent(): List<Event> =
+        eventApiService.getAllEvent()
+
+    override suspend fun insertEvent(event: Event) {
+        eventApiService.insertEvent(event)
+    }
+
+    override suspend fun updateEvent(id_event: String, event: Event) {
+        eventApiService.updateEvent(id_event, event)
+    }
+
+    override suspend fun deleteEvent(id_event: String) {
+        try{
+            val response = eventApiService.deleteEvent(id_event)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Event. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getEventById(id_event: String): Event {
+        return eventApiService.getEventById(id_event)
+    }
+
 }
