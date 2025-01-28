349fc506758eedd4308f5bd804c34204aa98be17
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 10:00:19 +0700

    membuat fun PesertaCard

---
+++ app/src/main/java/com/example/finalproject/ui/view/Peserta/HomePesertaView.kt
@@ -0,0 +1,86 @@
+package com.example.finalproject.ui.view.Peserta
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.finalproject.model.Peserta
+
+@Composable
+fun PesertaCard(
+    peserta: Peserta,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Peserta) -> Unit = {}
+){
+    var deleteConfirmationRequared by rememberSaveable { mutableStateOf(false) }
+    Card (
+        modifier = modifier,
+        shape = MaterialTheme.shapes.small,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = peserta.id_peserta,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { deleteConfirmationRequared = true }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null
+                    )
+                }
+                if(deleteConfirmationRequared){
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            onDeleteClick(peserta)
+                            deleteConfirmationRequared = false
+                        },
+                        onDeleteCancel = { deleteConfirmationRequared = false }
+                    )
+                }
+            }
+            Text(
+                text = peserta.nama_peserta,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = peserta.email,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = peserta.nomor_telepon,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
