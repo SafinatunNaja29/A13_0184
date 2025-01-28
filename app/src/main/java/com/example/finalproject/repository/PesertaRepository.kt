22c94a958e5cc2828a2722b0e46dc28144739869
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 09:35:36 +0700

    menambahkan class NetworkPesertaRepository

--- app/src/main/java/com/example/finalproject/repository/PesertaRepository.kt
+++ app/src/main/java/com/example/finalproject/repository/PesertaRepository.kt
@@ -11,3 +11,37 @@ interface PesertaRepository{
     suspend fun getPesertaById(id_peserta: String): Peserta
 }
 
+class NetworkPesertaRepository(
+    private val pesertaApiService: PesertaService
+): PesertaRepository {
+    override suspend fun getPeserta(): List<Peserta> =
+        pesertaApiService.getAllPeserta()
+
+    override suspend fun insertPeserta(peserta: Peserta) {
+        pesertaApiService.insertPeserta(peserta)
+    }
+
+    override suspend fun updatePeserta(id_peserta: String, peserta: Peserta) {
+        pesertaApiService.updatePeserta(id_peserta, peserta)
+    }
+
+    override suspend fun deletePeserta(id_peserta: String) {
+        try{
+            val response = pesertaApiService.deletePeserta(id_peserta)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Peserta. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getPesertaById(id_peserta: String): Peserta {
+        return pesertaApiService.getPesertaById(id_peserta)
+    }
+
+}
