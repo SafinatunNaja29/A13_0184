3af9b9b9d7bd729781a561bec4412074935a63e4
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 13:30:56 +0700

    membuat dataclass Transaksi

---
+++ app/src/main/java/com/example/finalproject/model/Transaksi.kt
@@ -0,0 +1,14 @@
+package com.example.finalproject.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Transaksi(
+    val id_transaksi: String,
+    val id_tiket: String? = null,
+    val nama_event: String,
+    val nama_peserta: String,
+    val jumlah_tiket: String,
+    val jumlah_pembayaran: String,
+    val tanggal_transaksi: String,
+)
