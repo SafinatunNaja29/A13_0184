d580ff82391edb702a61c95208ca3e79e58da3e4
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-28 10:14:32 +0700

    membuat fun EntryPesertaScreen

--- app/src/main/java/com/example/finalproject/ui/view/Peserta/InsertPesertaView.kt
+++ app/src/main/java/com/example/finalproject/ui/view/Peserta/InsertPesertaView.kt
@@ -4,18 +4,64 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject.ui.viewmodel.Peserta.InsertPesertaUiEvent
 import com.example.finalproject.ui.viewmodel.Peserta.InsertPesertaUiState
+import com.example.finalproject.ui.viewmodel.Peserta.InsertPesertaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPesertaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPesertaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                canNavigateBack = true,
+                scrollBehavior = scrollBehaviour,
+                navigateUp = navigateBack,
+                judul = "Insert Data Peserta"
+            )
+        }
+    ) { innerPadding ->
+        EntryBodyPeserta(
+            insertPesertaUiState = viewModel.uiPesertaState,
+            onPesertaValueChange = viewModel::updateInsertPesertaState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPeserta()
+                    navigateBack()
+                }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBodyPeserta(
